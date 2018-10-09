8286e72bc29ff7cc5c6bbd9d56940c36a22c5cf5
Pratibha Barsale <pbarsale@buffalo.edu>
2018-10-09 00:38:38 -0400

    MVC model

---
+++ includes/dbh.inc.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbServername = "tethys.cse.buffalo.edu";
+$dbUsername = "pnaik5";
+$dbPassword = "ChangeMe";
+$dbName = "museum_db";
+
+$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
